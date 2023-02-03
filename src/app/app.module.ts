28935d5b9e351854f7a60acbd726526f5ea63a7a
Stephan Nell <nellstephanj@gmail.com>
2023-02-03 16:37:03 +0100

    Split components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LeafletModule} from "@asymmetrik/ngx-leaflet";
-import { WorldMapComponent } from './core/world-map/world-map/world-map.component';
+import { WorldMapComponent } from './core/world-map/world-map.component';
+import {RouterModule} from "@angular/router";
+import { HomeComponent } from './core/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WorldMapComponent
+    WorldMapComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     LeafletModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent, pathMatch: 'full'},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
